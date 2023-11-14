28e16a1a8e976e40477256e1e4128f2e2c726bcb
Sonnie <mgathoni.gathoni9@gmail.com>
2023-11-14 11:02:51 +0300

    update

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import mean_squared_error
-import numpy as np
 
 # Load data
 plant_df = pd.read_csv("plant_data.csv")
@@ -16,41 +8,26 @@ animal_df = pd.read_csv("animal_data.csv")
 # Sidebar for user input
 st.sidebar.title("User Input")
 
-# Plant Prediction Section
+# Plant Prediction Section in the sidebar
 st.sidebar.subheader("Plant Prediction")
 plant_input = {}
 
 # Assuming 'Plant_Disease_Management', 'Pest_Management', etc. are the features in your dataset
-plant_input["Plant"] = st.sidebar.selectbox("Plant", plant_df["Plant"].unique())
+plant_input["Plant"] = st.sidebar.selectbox(
+    "Plant", plant_df["Plant"].unique(), key="plant_selectbox"
+)
 plant_input["Plant_Disease_Management"] = st.sidebar.selectbox(
-    "Disease Management", plant_df["Plant_Disease_Management"].unique()
+    "Disease Management",
+    plant_df["Plant_Disease_Management"].unique(),
+    key="disease_management_selectbox",
 )
 plant_input["Pest_Management"] = st.sidebar.selectbox(
-    "Pest Management", plant_df["Pest_Management"].unique()
+    "Pest Management",
+    plant_df["Pest_Management"].unique(),
+    key="pest_management_selectbox",
 )
 
-# Animal Prediction Section
-st.sidebar.subheader("Animal Prediction")
-animal_input = {}
-
-# Assuming 'Animal_Group', 'Animal_Type', etc. are the features in your dataset
-animal_input["Animal_Group"] = st.sidebar.selectbox(
-    "Animal Group", animal_df["Animal_Group"].unique()
-)
-animal_input["Animal_Type"] = st.sidebar.selectbox(
-    "Animal Type", animal_df["Animal_Type"].unique()
-)
-animal_input["Animal_Diseases_Management"] = st.sidebar.selectbox(
-    "Diseases Management", animal_df["Animal_Diseases_Management"].unique()
-)
-animal_input["Disease_Type"] = st.sidebar.selectbox(
-    "Disease", animal_df["Disease_Type"].unique()
-)
-animal_input["Disease_Treatment"] = st.sidebar.selectbox(
-    "Diseases treatment", animal_df["Disease_Treatment"].unique()
-)
-
-# Plant Prediction Section
+# Plant Prediction Section in the main section
 st.header("Plant Prediction")
 
 # Plant Prediction input from sidebar
@@ -58,13 +35,17 @@ plant_prediction_input = {}
 
 # Assuming 'Plant_Disease_Management', 'Pest_Management', etc. are the features in your dataset
 plant_prediction_input["Plant"] = st.sidebar.selectbox(
-    "Plant", plant_df["Plant"].unique()
+    "Plant", plant_df["Plant"].unique(), key="main_plant_selectbox"
 )
 plant_prediction_input["Plant_Disease_Management"] = st.sidebar.selectbox(
-    "Disease Management", plant_df["Plant_Disease_Management"].unique()
+    "Disease Management",
+    plant_df["Plant_Disease_Management"].unique(),
+    key="main_disease_management_selectbox",
 )
 plant_prediction_input["Pest_Management"] = st.sidebar.selectbox(
-    "Pest Management", plant_df["Pest_Management"].unique()
+    "Pest Management",
+    plant_df["Pest_Management"].unique(),
+    key="main_pest_management_selectbox",
 )
 
 # Display user input for plant prediction
@@ -75,7 +56,32 @@ st.write(plant_prediction_input)
 st.subheader("Predicted Plant Harvest:")
 st.write("Replace this with your prediction logic")
 
-# Animal Prediction Section
+# Animal Prediction Section in the sidebar
+st.sidebar.subheader("Animal Prediction")
+animal_input = {}
+
+# Assuming 'Animal_Group', 'Animal_Type', etc. are the features in your dataset
+animal_input["Animal_Group"] = st.sidebar.selectbox(
+    "Animal Group", animal_df["Animal_Group"].unique(), key="animal_group_selectbox"
+)
+animal_input["Animal_Type"] = st.sidebar.selectbox(
+    "Animal Type", animal_df["Animal_Type"].unique(), key="animal_type_selectbox"
+)
+animal_input["Animal_Diseases_Management"] = st.sidebar.selectbox(
+    "Diseases Management",
+    animal_df["Animal_Diseases_Management"].unique(),
+    key="animal_disease_management_selectbox",
+)
+animal_input["Disease_Type"] = st.sidebar.selectbox(
+    "Disease", animal_df["Disease_Type"].unique(), key="disease_type_selectbox"
+)
+animal_input["Disease_Treatment"] = st.sidebar.selectbox(
+    "Diseases treatment",
+    animal_df["Disease_Treatment"].unique(),
+    key="disease_treatment_selectbox",
+)
+
+# Animal Prediction Section in the main section
 st.header("Animal Prediction")
 
 # Animal Prediction input from sidebar
@@ -83,19 +89,25 @@ animal_prediction_input = {}
 
 # Assuming 'Animal_Group', 'Animal_Type', etc. are the features in your dataset
 animal_prediction_input["Animal_Group"] = st.sidebar.selectbox(
-    "Animal Group", animal_df["Animal_Group"].unique()
+    "Animal Group",
+    animal_df["Animal_Group"].unique(),
+    key="main_animal_group_selectbox",
 )
 animal_prediction_input["Animal_Type"] = st.sidebar.selectbox(
-    "Animal Type", animal_df["Animal_Type"].unique()
+    "Animal Type", animal_df["Animal_Type"].unique(), key="main_animal_type_selectbox"
 )
 animal_prediction_input["Animal_Diseases_Management"] = st.sidebar.selectbox(
-    "Diseases Management", animal_df["Animal_Diseases_Management"].unique()
+    "Diseases Management",
+    animal_df["Animal_Diseases_Management"].unique(),
+    key="main_animal_disease_management_selectbox",
 )
 animal_prediction_input["Disease_Type"] = st.sidebar.selectbox(
-    "Disease", animal_df["Disease_Type"].unique()
+    "Disease", animal_df["Disease_Type"].unique(), key="main_disease_type_selectbox"
 )
 animal_prediction_input["Disease_Treatment"] = st.sidebar.selectbox(
-    "Diseases treatment", animal_df["Disease_Treatment"].unique()
+    "Diseases treatment",
+    animal_df["Disease_Treatment"].unique(),
+    key="main_disease_treatment_selectbox",
 )
 
 # Display user input for animal prediction
